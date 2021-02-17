3927bfc72dcff970e22ecec7d5d9f17af3c80196
Alpha-chatbot <w1486991950@163.com>
2021-02-17 22:35:37 +0800

    second commit

---
+++ src/main/java/com/wang/book/dao/LendingMapper.java
@@ -0,0 +1,17 @@
+package com.wang.book.dao;
+
+import com.wang.book.pojo.Lending;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface LendingMapper {
+
+
+    /* === 借阅相关 ===*/
+    //通过图书ID和学生ID预约书籍，并插入
+    int insertLending(@Param("bookId") int bookId, @Param("studentId") int studentId);
+    //通过学生ID可以查询已预约的书
+    List<Lending> queryLending(int studentId);
+
+}
---
+++ src/main/java/com/wang/book/service/BookService.java
@@ -0,0 +1,32 @@
+package com.wang.book.service;
+
+import com.wang.book.dto.LendingState;
+import com.wang.book.pojo.Books;
+import com.wang.book.pojo.Lending;
+import com.wang.book.pojo.Student;
+
+import java.util.List;
+
+public interface BookService {
+
+
+        //根据bookID查询
+        Books getById(int bookId);
+
+        //查询所有书籍
+        List<Books> getList();
+
+        //登陆时查询数据库是否有该学生记录
+        Student validateStu(int studentId, int password);
+
+        //按照图书名称查询
+        List<Books> getSomeList(String name);
+
+        // 查看某学生预约的所有图书
+        List<Lending> getLendingByStu(int studentId);
+        //利用学生ID来预约图书
+
+        LendingState lendingState(int bookId, int studentId);
+
+    }
+
